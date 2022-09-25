e13a3fee5c608d9c1da95f8da1e846e1d2117763
rayyanmhd77 <rayyan.moh8@gmail.com>
2022-09-25 20:02:26 +0200

    fin du header

---
+++ amazon.principal.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="amazon.principal.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+</head>
+
+<body>
+  <nav>
+    <div class="left dflex">
+      <a href="#"><img src="amazon.png" alt="logo" class="logo" /><span>.in</span></a>
+
+      <div class="item">
+        <p><i class="fa fa-map-marker me"></i>
+        bonjour,</p>
+        <h5>Entrer votre adresse</h5>
+      </div>
+    </div>
+    <div class="searchBox dflex">
+      <div class="box">
+        <div class="dflex">
+          <button type="submit" class="all">
+            Toutes nos catégories <i class="fa fa-sort-desc"></i>
+          </button>
+          <input type="text" />
+          <button type="submit" class="search">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="right dflex">
+      <div class="item">
+        <i class="fa fa-flag"></i><i class="fa fa-sort-desc"></i>
+      </div>
+      <div class="item">
+        <p>Bonjour, Identifiez-vous</p>
+        <h5>Compte et listes<i class="fa fa-sort-desc"></i></h5>
+      </div>
+      <div class="item">
+        <p>Retours</p>
+        <h5>et commandes</h5>
+      </div>
+      <div class="item"><i class="fa fa-shopping-cart fa-2x"></i>Panier</div>
+    </div>
+  </nav>
+  <nav class="subnavigation">
+    <ul class="submenu">
+      <a href="#"><i class="fa fa-bars"></i>
+        <li>Toutes</li>
+      </a>
+      <a href="#">
+        <li>Meilleurs ventes</li>
+      </a>
+      <a href="#">
+        <li>Amazon Basics</li>
+      </a>
+      <a href="#">
+        <li>Prime <i class="fa fa-sort-desc"></i></li>
+      </a>
+      <a href="#">
+        <li>Musique</li>
+      </a>
+      <a href="#">
+        <li>Dernière Nouveautés</s></li>
+      </a>
+      <a href="#">
+        <li>Service Client</li>
+      </a>
+      <a href="#">
+        <li>Ventes Flash</li>
+      </a>
+      <a href="#">
+        <li>Ebooks kindle</li>
+      </a>
+      <a href="#">
+        <li>Livres</li>
+      </a>
+      <a href="#">
+        <li>Audible</li>
+      </a>
+      </a>
+    </ul>
+    <ul>
+      <a href="#"><i class="fa fa-amazon bg"></i>
+        <li >Profitez des Ventes Flash Exclusives Prime</li>
+      </a>
+    </ul>
+  </nav>
+  <section></section>
+</body>
+
+</html>
